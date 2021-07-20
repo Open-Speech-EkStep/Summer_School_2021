cd5335c0fbeb4d119e5f8a5ba8ea61738cd9266b
agupta54 <anirudhgupta12@gmail.com>
2021-07-20 15:30:44 +0000

    data loader with transforms

--- language_identification/src/data_loader.py
+++ language_identification/src/data_loader.py
@@ -1,12 +1,17 @@
 from torch.utils.data import Dataset
 import pandas as pd
 import torchaudio
+import torch
+from parameters import SAMPLE_RATE, FEATURE_EXTRACTION, DATA_FILE, CROP_DURATION
 
 class LanguageIdentificationDataset(Dataset):
 
-    def __init__(self, data_file):
+    def __init__(self):
 
-        self.data_file = pd.read_csv(data_file)
+        self.data_file = pd.read_csv(DATA_FILE)
+        self.feature_extraction = FEATURE_EXTRACTION
+        self.crop_duration = CROP_DURATION
+        self.sample_rate = SAMPLE_RATE
 
 
     def __len__(self):
@@ -17,22 +22,55 @@ class LanguageIdentificationDataset(Dataset):
     
     def get_audio_language_label(self, idx):
         return self.data_file['label'][idx]
+    
+    def audio_transform(self, raw_audio):
+
+        if self.feature_extraction == 'raw audio':
+            return raw_audio
+
+        elif self.feature_extraction == 'mel spectrogram':
+            return torchaudio.transforms.MelSpectrogram(
+                sample_rate=self.sample_rate,
+                n_fft=1024,
+                hop_length=512,
+                n_mels=64
+            )(raw_audio)
+
+        else:
+            print("Incorrect feature extraction method")
+
+    
+    def crop_or_pad_audio(self, raw_audio):
+
+        num_frames = self.crop_duration * self.sample_rate
+
+        if raw_audio.shape[1] >= num_frames:
+            raw_audio = raw_audio[:, :num_frames]
+            return raw_audio
+
+        else:
+            padding_dimension = (0, num_frames - raw_audio.shape[1])
+            return torch.nn.functional.pad(raw_audio, padding_dimension)
+
 
     def __getitem__(self, idx):
         audio_path = self.get_audio_path(idx)
         language_label = self.get_audio_language_label(idx)
 
-        raw_audio, sr = torchaudio.load(audio_path)
+        audio_signal, _ = torchaudio.load(audio_path)
+        audio_signal = self.crop_or_pad_audio(audio_signal)
+        audio_signal = self.audio_transform(audio_signal)
 
-        return raw_audio, language_label
+        return audio_signal, language_label
+        
 
 if __name__ == "__main__":
-    lid = LanguageIdentificationDataset('data/audio_data.csv')
-    print(len(lid))
+    lid = LanguageIdentificationDataset()
 
     s, l = lid[0]
     print(s)
-    print(l)
+    print(s.shape)
+
 
 
 
---
+++ language_identification/src/parameters.py
@@ -0,0 +1,4 @@
+SAMPLE_RATE = 16000
+FEATURE_EXTRACTION = 'raw audio' # 'raw audio' 'mel spectrogram'
+DATA_FILE = 'data/audio_data.csv'
+CROP_DURATION = 4
